b3cfc5eab8d2d2fb03946e972c72ec2fdecdd710
hemantChandalia <154237393+hemantChandalia@users.noreply.github.com>
2024-03-08 12:36:19 +0530

    textutils

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import TextForm from './components/TextForm';
-
+import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
-     
-   {/* <Navbar title = "Textutils" aboutText = "About Textutils"/> */}
-   <Navbar/>
-   <TextForm/>
-</> 
-
+      {/* <Navbar title = "Textutils" aboutText = "About Textutils"/> */}
+      {/* <Navbar/> */}
+      <Navbar title="TextUtils" />
+      <div className="container my-3">
+        <TextForm heading="Enter the text to analyze below" />
+      </div>
+    </>
   );
 }
 
--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function TextForm() {
+export default function TextForm(props) {
+  const [text, setText] = useState('Enter text here');
+  console.log("Uppercase was clicked")
+
+  const handleUpClick = ()=>{
+    console.log("on click")
+  }
+
+  const handleOnChange = ()=>{
+    console.log("on change")
+  }
+// tex"new text"; // wrong way to change the state
+// setText("new text"); // Correct way to change the statet = 
   return (
     
-    
+   <div>
+    <h1>{props.heading}</h1>
 <div className="mb-3">
-  <label for="myBox" className="form-label">Example textarea</label>
-  <textarea className="form-control" id="myBox" rows="3"></textarea>
-</div>
     
+
+  <textarea value={text} className="form-control" onChange={handleOnChange} id="myBox" rows="10"></textarea>
+</div>
+<button className='btn-btn-primary' onClick={handleUpClick}>Covert to Uppercase</button>
+</div>
   )
 }
